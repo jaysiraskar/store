8ca13e46bba97b6726f841077f1179f69063daf7
Jay Siraskar <Jay.Siraskar@spacestem.com>
2023-08-30 20:38:04 +0530

    Angular shop view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { IProduct } from 'src/app/models/product';
-import { IPagination } from 'src/app/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +8,10 @@ import { IPagination } from 'src/app/models/pagination';
 
 export class AppComponent implements OnInit {
   title = 'Store';
-  products: IProduct[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5081/api/products?pageSize=50').subscribe(
-      (response: any) => {
-        const paginationResponse = response as IPagination;
-        this.products = paginationResponse.data;
-      },
-      (error) => {
-        console.error('API Request Error:', error);
-      }
-    );
+
   }
 }
