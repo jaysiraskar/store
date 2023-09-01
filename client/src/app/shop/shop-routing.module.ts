24b67067bb2fbba881b663aae977cf94e43d2398
Jay Siraskar <Jay.Siraskar@spacestem.com>
2023-09-01 11:00:43 +0530

    fixed issues

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
+const routes: Routes = [
+  {path: '', component: ShopComponent},
+  {path: ':id', component: ProductDetailsComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
     RouterModule
   ]
 })
