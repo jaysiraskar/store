50fcd8aa68ebf6fe6533e1de558856076dc034d2
Jay Siraskar <Jay.Siraskar@spacestem.com>
2023-09-06 11:52:10 +0530

    login functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent implements OnInit {
   title = 'Store';
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
-const basketId = localStorage.getItem('basket_id');
-if(basketId){
-  this.basketService.getBasket(basketId).subscribe(() => {
-    console.log('initialised basket');
-  }, error => {
-    console.log(error);
-  });
-}
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if(token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialised basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { RouterModule } from '@angular/router';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ReactiveFormsModule
   ],
   exports: [
     PaginationModule,
@@ -21,7 +26,10 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
     PagerComponent,
     RouterModule,
     CarouselModule,
-    OrderTotalsComponent
+    BsDropdownModule,
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
