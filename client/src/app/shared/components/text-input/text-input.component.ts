4cc07fae4951d74f787f28c7f00465f7257ed37e
Jay Siraskar <Jay.Siraskar@spacestem.com>
2023-09-07 10:46:24 +0530

    login comonent

--- client/src/app/shared/components/text-input/text-input.component.ts
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit, ViewChild, ElementRef, Input, Self } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  Self,
+} from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.scss']
+  styleUrls: ['./text-input.component.scss'],
 })
 export class TextInputComponent implements OnInit, ControlValueAccessor {
   @ViewChild('input', { static: true }) input!: ElementRef;
@@ -18,16 +25,18 @@ export class TextInputComponent implements OnInit, ControlValueAccessor {
   ngOnInit() {
     const control = this.controlDir.control!;
     const validators = control.validator ? [control.validator] : [];
-    const asyncValidators = control.asyncValidator ? [control.asyncValidator] : [];
+    const asyncValidators = control.asyncValidator
+      ? [control.asyncValidator]
+      : [];
 
     control.setValidators(validators);
     control.setAsyncValidators(asyncValidators);
     control.updateValueAndValidity();
   }
 
-  onChange(event: any) { }
+  onChange(event: any) {}
 
-  onTouched() { }
+  onTouched() {}
 
   writeValue(obj: any): void {
     this.input.nativeElement.value = obj || '';
@@ -40,5 +49,4 @@ export class TextInputComponent implements OnInit, ControlValueAccessor {
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
-
 }
